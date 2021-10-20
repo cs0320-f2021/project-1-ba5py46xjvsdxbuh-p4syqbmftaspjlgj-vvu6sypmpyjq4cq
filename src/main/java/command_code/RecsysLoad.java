29a8504a44980de0b0db87c9ac8d205411a268ee
L-aces <andrew_alper@brown.edu>
2021-10-20 13:33:30 -0400

    recsys load finished

--- src/main/java/command_code/RecsysLoad.java
+++ src/main/java/command_code/RecsysLoad.java
@@ -31,12 +31,25 @@ public class RecsysLoad implements REPLCallable {
       List<Negative> negatives = db.select(Negative.class, new HashMap<String, String>());
       skills = db.select(Skills.class, new HashMap<String, String>());
       tree = new KDTree(skills.get(0).getSkills().length);
-      filter = new BloomFilterRecommender<Item>(traits, 0.01);
-      //filter.setBloomFilterComparator(new XnorSimilarityComparator());
       tree.loadData(skills);
       System.out.println(interests);
       for (int i = 0; i < interests.size(); i++) {
+        if (traits.containsKey(interests.get(i).getID())) {
+          ((Trait) traits.get(interests.get(i).getID())).addToData(interests.get(i).getInterest());
+        } else {
+          Trait insert = new Trait(interests.get(i));
+          traits.put(insert.getId(), insert);
+        }
       }
+      for (int i = 0; i < positives.size(); i++) {
+        ((Trait) traits.get(positives.get(i).getID())).addToData(positives.get(i).getTrait());
+      }
+      for (int i = 0; i < negatives.size(); i++) {
+        ((Trait) traits.get(negatives.get(i).getID())).addToData(negatives.get(i).getTrait());
+      }
+      filter = new BloomFilterRecommender<Item>(traits, 0.01);
+      BloomFilter<String> f = new BloomFilter<>(0.01, traits.size());
+      filter.setBloomFilterComparator(new XnorSimilarityComparator(f));
     } catch (Exception e) {
       System.out.println("Error: this request could not be processed");
       System.out.println(e.toString());
